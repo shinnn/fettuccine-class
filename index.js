f09c1c2e730f2bb30809374351c8a9166e9f8e84
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-30 19:46:02 +0900

    update dependencies and devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -10,62 +10,63 @@ const loadRequest = loadRequestFromNpmOrCwd();
 const userAgentHeaderRe = /^user-agent$/i;
 
 function isUserAgentHeader(key) {
-  return userAgentHeaderRe.test(key);
+	return userAgentHeaderRe.test(key);
 }
 
 class Fettuccine {
-  constructor(options) {
-    this.options = Object.assign({gzip: true}, options);
+	constructor(options) {
+		this.options = Object.assign({gzip: true}, options);
 
-    if (this.options.headers) {
-      const hasUserAgent = Object.keys(this.options.headers).some(isUserAgentHeader);
+		if (this.options.headers) {
+			const hasUserAgent = Object.keys(this.options.headers).some(isUserAgentHeader);
 
-      if (!hasUserAgent) {
-        this.options.headers['user-agent'] = DEFAULT_USER_AGENT;
-      }
-    } else {
-      this.options.headers = {'user-agent': DEFAULT_USER_AGENT};
-    }
+			if (!hasUserAgent) {
+				this.options.headers['user-agent'] = DEFAULT_USER_AGENT;
+			}
+		} else {
+			this.options.headers = {'user-agent': DEFAULT_USER_AGENT};
+		}
 
-    this.request = loadRequest;
-  }
-  get(url, options) {
-    if (typeof url !== 'string') {
-      return Promise.reject(new TypeError(`${util.inspect(url)} is not a string. Expected a URI.`));
-    }
+		this.request = loadRequest;
+	}
 
-    options = Object.assign({}, this.options, options);
-    options.headers = Object.assign({}, this.options.headers, options.headers);
+	get(url, options) {
+		if (typeof url !== 'string') {
+			return Promise.reject(new TypeError(`${util.inspect(url)} is not a string. Expected a URI.`));
+		}
 
-    if (!options.baseUrl && url === '') {
-      return Promise.reject(new Error('Expected a URI but received an empty string.'));
-    }
+		options = Object.assign({}, this.options, options);
+		options.headers = Object.assign({}, this.options.headers, options.headers);
 
-    return this.request.then(function promisifyRequest(request) {
-      return new Promise(function executor(resolve, reject) {
-        request(url, options, function requestCallback(err, res) {
-          if (err) {
-            reject(err);
-            return;
-          }
+		if (!options.baseUrl && url === '') {
+			return Promise.reject(new Error('Expected a URI but received an empty string.'));
+		}
 
-          resolve(res);
-        });
-      });
-    });
-  }
+		return this.request.then(function promisifyRequest(request) {
+			return new Promise(function executor(resolve, reject) {
+				request(url, options, function requestCallback(err, res) {
+					if (err) {
+						reject(err);
+						return;
+					}
+
+					resolve(res);
+				});
+			});
+		});
+	}
 }
 
 [
-  'post',
-  'put',
-  'patch',
-  'head',
-  'delete'
+	'post',
+	'put',
+	'patch',
+	'head',
+	'delete'
 ].forEach(method => {
-  Fettuccine.prototype[method] = function(url, options) {
-    return this.get(url, Object.assign({}, options, {method}));
-  };
+	Fettuccine.prototype[method] = function(url, options) {
+		return this.get(url, Object.assign({}, options, {method}));
+	};
 });
 
 module.exports = Fettuccine;
--- test.js
+++ test.js
@@ -1,80 +1,80 @@
 'use strict';
 
-const http = require('http');
+const {createServer} = require('http');
 
 const Fettuccine = require('.');
 const test = require('tape');
 
-const server = http.createServer(function(req, response) {
-  if (req.method === 'POST') {
-    req.once('data', data => {
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.end(`{"data": ${data.toString()}}`);
-    });
-  } else if (req.method === 'DELETE') {
-    response.writeHead(200, {
-      'Content-Type': 'text/plain',
-      foo: req.headers['user-agent']
-    });
-    response.end('{"status": "deleted"}');
-  } else {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end('[1]');
-  }
+const server = createServer(function(req, response) {
+	if (req.method === 'POST') {
+		req.once('data', data => {
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(`{"data": ${data.toString()}}`);
+		});
+	} else if (req.method === 'DELETE') {
+		response.writeHead(200, {
+			'Content-Type': 'text/plain',
+			foo: req.headers['user-agent']
+		});
+		response.end('{"status": "deleted"}');
+	} else {
+		response.writeHead(200, {'Content-Type': 'application/json'});
+		response.end('[1]');
+	}
 
-  if (++this.responded === 3) {
-    this.close();
-  }
+	if (++this.responded === 3) {
+		this.close();
+	}
 }).listen(8124);
 
 server.responded = 0;
 
 test('Fettuccine()', t => {
-  t.plan(7);
+	t.plan(7);
 
-  new Fettuccine().get('http://localhost:8124').then(response => {
-    t.strictEqual(response.body, '[1]', 'should send a request to the server.');
-  }).catch(t.fail);
+	new Fettuccine().get('http://localhost:8124').then(response => {
+		t.equal(response.body, '[1]', 'should send a request to the server.');
+	}).catch(t.fail);
 
-  new Fettuccine({
-    method: 'post',
-    body: [1, 2],
-    headers: {},
-    json: true
-  }).get('http://localhost:8124').then(response => {
-    t.deepEqual(response.body, {data: [1, 2]}, 'should use constructor parameters as its default options.');
-  }).catch(t.fail);
+	new Fettuccine({
+		method: 'post',
+		body: [1, 2],
+		headers: {},
+		json: true
+	}).get('http://localhost:8124').then(response => {
+		t.deepEqual(response.body, {data: [1, 2]}, 'should use constructor parameters as its default options.');
+	}).catch(t.fail);
 
-  new Fettuccine({headers: {'User-agent': 'fettucine'}}).delete('', {
-    headers: {'user-Agent': 'Fettucine'},
-    json: false,
-    baseUrl: 'http://localhost:8124'
-  }).then(response => {
-    t.strictEqual(response.body, '{"status": "deleted"}', 'should have alias methods.');
-    t.strictEqual(
-      response.headers.foo,
-      'Fettucine',
-      'should use the first argument of the method as prior options.'
-    );
-  }).catch(t.fail);
+	new Fettuccine({headers: {'User-agent': 'fettucine'}}).delete('', {
+		headers: {'user-Agent': 'Fettucine'},
+		json: false,
+		baseUrl: 'http://localhost:8124'
+	}).then(response => {
+		t.equal(response.body, '{"status": "deleted"}', 'should have alias methods.');
+		t.equal(
+			response.headers.foo,
+			'Fettucine',
+			'should use the first argument of the method as prior options.'
+		);
+	}).catch(t.fail);
 
-  new Fettuccine().patch('https://n/o/_/s/e/r/v/e/r').then(t.fail, err => {
-    t.equal(err.syscall, 'getaddrinfo', 'should be rejected when the request fails.');
-  }).catch(t.fail);
+	new Fettuccine().patch('https://n/o/_/s/e/r/v/e/r').then(t.fail, err => {
+		t.equal(err.syscall, 'getaddrinfo', 'should be rejected when the request fails.');
+	}).catch(t.fail);
 
-  new Fettuccine().head(['1', true]).then(t.fail, err => {
-    t.equal(
-      err.message,
-      '[ \'1\', true ] is not a string. Expected a URI.',
-      'should be rejected when the URL is not a string.'
-    );
-  }).catch(t.fail);
+	new Fettuccine().head(['1', true]).then(t.fail, err => {
+		t.equal(
+			err.message,
+			'[ \'1\', true ] is not a string. Expected a URI.',
+			'should be rejected when the URL is not a string.'
+		);
+	}).catch(t.fail);
 
-  new Fettuccine().delete('').then(t.fail, err => {
-    t.equal(
-      err.message,
-      'Expected a URI but received an empty string.',
-      'should be rejected when it takes an empty string URL and `baseUrl` is not defined.'
-    );
-  }).catch(t.fail);
+	new Fettuccine().delete('').then(t.fail, err => {
+		t.equal(
+			err.message,
+			'Expected a URI but received an empty string.',
+			'should be rejected when it takes an empty string URL and `baseUrl` is not defined.'
+		);
+	}).catch(t.fail);
 });
